82c509aa522e53d6db5823496f5405122aa32c7e
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-02-15 16:28:54 -0500

    added artsdata-pipeline-action to workflow

--- src/sitemap_scraping.rb
+++ src/sitemap_scraping.rb
@@ -35,6 +35,8 @@ end
 
 sparql_paths = [
   "./src/sparql/fix-date.sparql",
+  "./src/sparql/fix-start-dates.sparql",
+  "./src/sparql/fix-end-dates.sparql",
   "./src/sparql/fix-event-status.sparql",
   "./src/sparql/fix-places.sparql",
   "./src/sparql/make-uris.sparql",
