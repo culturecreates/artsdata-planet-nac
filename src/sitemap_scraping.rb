0b0d63e8ff07d60ad883ac6d39e6ce1a70c35c65
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-02-15 20:25:39 -0500

    load all entity_urls

--- src/sitemap_scraping.rb
+++ src/sitemap_scraping.rb
@@ -25,7 +25,7 @@ sitemap_xml = Nokogiri::XML(URI.open(sitemap_url))
 ns = { 'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9' }
 entity_urls = sitemap_xml.xpath('//xmlns:url[starts-with(xmlns:loc, "https://nac-cna.ca/en/event/")]/xmlns:loc', ns).map(&:text)
 puts "entity_urls: #{entity_urls}"
-entity_urls[1..25].each do |entity_url|
+entity_urls.each do |entity_url|
   begin
     entity_url = entity_url.gsub(' ', '+')
     graph << RDF::Graph.load(entity_url)
