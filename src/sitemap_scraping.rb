a808b00ed1ca7699c5214886e9ea0f4f3eae042a
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-05-15 08:47:37 -0400

    added correct path to sparql

--- src/sitemap_scraping.rb
+++ src/sitemap_scraping.rb
@@ -20,13 +20,14 @@ def perform_sparql_transformations(graph, sparql_paths)
   return graph
 end
 
+puts "Checking sitemap at #{sitemap_url}"
 sitemap_xml = Nokogiri::XML(URI.open(sitemap_url))
 # Extract URLs that start with 'https://nac-cna.ca/en/event/'
 ns = { 'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9' }
 entity_urls = sitemap_xml.xpath('//xmlns:url[starts-with(xmlns:loc, "https://nac-cna.ca/en/event/")]/xmlns:loc', ns).map(&:text)
 puts "entity_urls: #{entity_urls}"
 
-sparql_file = File.read('./sparql/add_derived_from.sparql')
+sparql_file = File.read('./src/sparql/add_derived_from.sparql')
 entity_urls.each do |entity_url|
   begin
     entity_url = entity_url.gsub(' ', '+')
@@ -51,3 +52,5 @@ graph = perform_sparql_transformations(graph, sparql_paths)
 File.open(file_name, 'w') do |file|
   file.puts(graph.dump(:jsonld))
 end
+
+puts "Saved JSON-LD to file #{file_name}"
