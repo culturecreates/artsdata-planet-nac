8d83ae0968dae24ef190c0c837e4abdcba24ff22
dev <dev.aravind@culturecreates.com>
2024-10-18 17:28:30 +0530

    Added SPARQL to remove organizations where name is empty

---
+++ tests/remove_empty_organizations.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'linkeddata'
+
+class ReplaceBlankNodesTest < Minitest::Test
+
+  def setup
+    @remove_empty_organizations_sparql_file = "./src/sparql/remove_empty_organizations.sparql"
+  end
+
+  # check that the blank node is replaced
+  def test_blank_node_replaced
+    sparql = SPARQL.parse(File.read(@remove_empty_organizations_sparql_file), update: true)
+    graph = RDF::Graph.load("./tests/fixtures/remove_empty_organizations.jsonld")
+    # puts "before: #{graph.dump(:jsonld)}"
+    graph.query(sparql)
+    # puts "after: #{graph.dump(:jsonld)}"
+    assert_equal 0, graph.query([nil, RDF::URI("http://schema.org/name"), ""]).count
+  end
+end
